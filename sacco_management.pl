0ef62ade7dbf1a03b58c963c63f8baa2783a92df
Shiloli-r <45619599+Shiloli-r@users.noreply.github.com>
2020-12-14 10:47:57 +0300

    Added Connection of Bus, Driver and Route

--- sacco_management.pl
+++ sacco_management.pl
@@ -1,13 +1,9 @@
-%%
 % CSC 312 Project
 % Question: Write a program that assists a medium sized transport sacco
 % to manage its data
 % Written by Ronald Onami, Ian Nalyanya and Casey Tiben
 %
 % ===> To start the program run start, which will give more information
-%
-%
-
 
 start:- write("\e[2J"), nl,
     write("Welcome to the Sacco Data Management System"),
@@ -20,6 +16,9 @@ start:- write("\e[2J"), nl,
     nl, write("Enter 'read_route' to enter route data "),
     nl, write("Enter 'delete_route' to delete route data "),
     nl, write("Enter 'showRoute' to show all routes "), nl,
+    nl, write("Enter 'connect' to connect bus, driver and route"),
+    nl, write("Enter 'showConnections' to show available connections "),nl,
+    nl, nl, write("ATTEMPTING TO VIEW RECORDS WITHOUT CREATING THEM MAY RESULT IN ERRORS!!!!"),nl,
     nl.
 
 %-----------------------  BUS MANAGEMENT -------------------------------
@@ -43,9 +42,6 @@ deleteBus(RegNumber) :- retract(bus(RegNumber)).
 showBus :- write("Registered Buses: "), nl, bus(_), forall(bus(X), printBus(X)).
 printBus(X):- format("---> bus(~w)", [X] ),nl.
 
-
-
-
 %-----------------  DRIVER MANAGEMENT ----------------------------------
 % Driver Data Entry
 read_driver :- getDriverDetails(Name),
@@ -56,7 +52,6 @@ getDriverDetails(Name) :- write("Enter Driver's Name (Enter 'quit' to stop): "),
 
 saveDriverDetails(Name) :- assertz(driver(Name)).
 
-
 % Driver Data Deletion
 delete_driver:- getDriverDetails(Name),
                 \+ Name = quit,
@@ -64,7 +59,6 @@ delete_driver:- getDriverDetails(Name),
 
 
 deleteDriver(Name):- retract(driver(Name)).
-
 
 %    SHOW DRIVER
 showDriver :- write("Registered Drivers: "), nl, forall(driver(Name), printDriver(Name)).
@@ -90,7 +84,17 @@ delete_route:- getRouteDetails(From, To),
                deleteRoute(From, To), delete_route.
 deleteRoute(From, To) :- retract(route(From, To)).
 
-
 %    SHOW ROUTE
 showRoute :- write("Registered Drivers: "), nl, forall(route(From, To), printRoute(From, To)).
 printRoute(From, To):- format("---> route(~w, ~w)", [From, To] ),nl.
+
+
+% --------------- CONNECT BUS, DRIVER AND ROUTE ---------------
+showConnections:- write("Available Connections: "),nl, forall(connections(Bus, Driver, From, To), format("====> connection(~w, ~w, ~w, ~w )", [Bus, Driver, From, To])).
+
+connect:- getConnection(Bus, Driver, From, To), saveConnection(Bus, Driver, From, To), nl.
+
+getConnection(Bus, Driver, From, To):- write("Enter The Bus: "),read(Bus),nl, write("Enter The Driver: "), read(Driver),nl,
+    write("From: "), read(From), nl, write("To: "), read(To), nl.
+
+saveConnection(Bus, Driver, From, To) :-  assertz(connections(Bus, Driver, From, To)),nl.
